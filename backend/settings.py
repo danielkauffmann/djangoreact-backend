74f9d1750df56efb5ede983813c74fa66588de3f
danielkauffmann <danielvenna2@gmail.com>
2020-08-22 20:20:38 -0300

    Adding postgres to settings

--- backend/settings.py
+++ backend/settings.py
@@ -69,9 +69,18 @@ CORS_ORIGIN_WHITELIST = [
 
 
 DATABASES = {
-    'default': {
+    'local': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djangoreact',
+        'USER': 'djangoreact',
+        'PASSWORD': 'kauffmann',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
