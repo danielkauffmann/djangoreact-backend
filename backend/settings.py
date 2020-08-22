e334e12fa62f4ad4ef0deb555f65e1d189658925
danielkauffmann <danielvenna2@gmail.com>
2020-08-22 19:59:55 -0300

    Fixing Cors whitelist

--- backend/settings.py
+++ backend/settings.py
@@ -61,7 +61,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
     'http://127.0.0.1:3000',
-    '191.234.162.100'
+    'http://191.234.162.100',
+    'http://191.234.162.100:8080'
 ]
 
 
